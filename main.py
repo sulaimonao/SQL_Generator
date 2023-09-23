e9647aeb613f072ed7a3304fb9d1df3b3caf2cf4
Akeem <16595684+sulaimonao@users.noreply.github.com>
2023-09-22 17:51:17 -0700

    Update main.py
    added .json modification to improve speed and cost.
    now prints only the SQL codes
    improved prompt code in "input_message"
    added db_cache function so support storing database values for repeated uses
    db_cache prompt users to select from stored database values
    Made sure to catch any potential API errors.
    Used a more efficient way to handle choices in the main menu.
    Checked the validity of the input data..

--- main.py
+++ main.py
@@ -3,124 +3,156 @@ import json
 from datetime import datetime
 from dotenv import load_dotenv
 import openai
+import re
 
 # Load API key from .env file
 load_dotenv()
 API_KEY = os.getenv('OPENAI_API_KEY')
 
+if not API_KEY:
+    raise ValueError("OPENAI_API_KEY is missing. Make sure it's set in the .env file.")
+
 # Initialize OpenAI client
 openai.api_key = API_KEY
 
-# Cache and memory
-cache = {}
-memory_file = "memory.json"
+# Memory and caching
+pycache_file = "pycache.json"
+db_cache_file = "db_cache.json"
+pycache = {}
+
+def load_json_file(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def save_json_file(file_name, data):
+    with open(file_name, 'w') as f:
+        json.dump(data, f)
+
+def load_pycache():
+    return load_json_file(pycache_file)
+
+def save_to_pycache(key, value):
+    pycache[key] = value
+    save_json_file(pycache_file, pycache)
+
+def load_db_cache():
+    return load_json_file(db_cache_file)
+
+def save_db_cache(db_data):
+    db_cache = load_db_cache()
+    db_cache.append(db_data)
+    save_json_file(db_cache_file, db_cache)
 
 def get_input_data():
-    db_name = input("Enter the name of the database (or type 'quit' to exit): ")
-    if db_name.lower() == 'quit':
-        return None
-    
-    num_schemas = input("Enter how many schemas are in the database (or type 'quit' to exit): ")
-    if num_schemas.lower() == 'quit':
-        return None
-    num_schemas = int(num_schemas)
+    db_cache = load_db_cache()
+    if db_cache:
+        print("Saved databases:")
+        for idx, db in enumerate(db_cache, 1):
+            print(f"{idx}. {db['db_name']}")
+
+        selection = input("Enter the number of a saved database, 'new' to input a new one, or 'quit' to exit: ").strip().lower()
+
+        if selection.isdigit() and 0 < int(selection) <= len(db_cache):
+            index = int(selection) - 1
+            return db_cache[index]['db_name'], db_cache[index]['schemas']
+        elif selection == 'quit':
+            return None, None
+        elif selection != 'new':
+            print("Invalid choice. Please enter a valid number, 'new', or 'quit'.")
+
+    db_name = input("Enter the name of the database: ")
+
+    try:
+        num_schemas = int(input("Enter how many schemas are in the database (or type 'quit' to exit): "))
+    except ValueError:
+        print("Please enter a valid number.")
+        return None, None
 
     schemas = {}
     for _ in range(num_schemas):
         schema_name = input("Enter the schema name (or type 'quit' to exit): ")
         if schema_name.lower() == 'quit':
-            return None
-        
-        num_columns = input(f"How many columns are in the {schema_name} schema? (or type 'quit' to exit): ")
-        if num_columns.lower() == 'quit':
-            return None
-        num_columns = int(num_columns)
+            return None, None
+
+        try:
+            num_columns = int(input(f"How many columns are in the {schema_name} schema? (or type 'quit' to exit): "))
+        except ValueError:
+            print("Please enter a valid number.")
+            return None, None
 
         columns = {}
         for _ in range(num_columns):
             column_name = input("Enter column name (or type 'quit' to exit): ")
             if column_name.lower() == 'quit':
-                return None
+                return None, None
 
             column_type = input("Is this column values are strings, floats, or integers? (or type 'quit' to exit): ")
-            if column_type.lower() == 'quit':
-                return None
-
+            if column_type.lower() not in ['strings', 'floats', 'integers']:
+                print("Invalid type.")
+                return None, None
             columns[column_name] = column_type
         schemas[schema_name] = columns
 
+    save_db = input("Would you like to save this database information for frequent use? (yes/no): ").strip().lower()
+    if save_db == "yes":
+        save_db_cache({"db_name": db_name, "schemas": schemas})
+
     return db_name, schemas
 
-def load_or_get_data():
-    if os.path.exists(memory_file):
-        with open(memory_file, 'r') as f:
-            return json.load(f)
-    else:
-        data = get_input_data()
-        if data is None:
-            return None
-        with open(memory_file, 'w') as f:
-            json.dump(data, f)
-        return data
-
 def generate_sql(db_data, prompt):
+    if prompt in pycache:
+        return pycache[prompt]
+
     db_name, schemas = db_data
-
-    if prompt in cache:
-        return cache[prompt]
-
-    schema_details = []
-    for schema, columns in schemas.items():
-        column_details = ', '.join([f"{col} ({type_})" for col, type_ in columns.items()])
-        schema_details.append(f"{schema}: {column_details}")
-
+    schema_details = [f"{schema}: {', '.join([f'{col} ({type_})' for col, type_ in columns.items()])}" for schema, columns in schemas.items()]
     schema_string = '\n'.join(schema_details)
 
     input_messages = [{
         "role": "system",
-        "content": "You are a helpful assistant."
+        "content": "You are a helpful assistant that understands SQL syntax and semantics perfectly."
     }, {
         "role": "user",
-        "content": f"I have a database named {db_name} with the following schemas and columns:\n"
-                   f"{schema_string}\n"
-                   f"How can I generate an SQL query to {prompt}?"
+        "content": f"I have a database named {db_name} with the following schemas and columns, and their exact data types:\n{schema_string}\nGenerate an exact SQL query to {prompt}. Ensure the syntax and semantics are correct."
     }]
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=input_messages
-    )
+    try:
+        response = openai.ChatCompletion.create(model="gpt-4", messages=input_messages)
 
-    sql = response.choices[0].message['content'].strip()
-    cache[prompt] = sql
-    return sql
+        match = re.search(r'```sql\n(.*?)\n```', response.choices[0].message['content'], re.DOTALL)
+        sql = match.group(1).strip() if match else "Failed to extract SQL."
+
+        save_to_pycache(prompt, sql)
+        return sql
+
+    except Exception as e:
+        return f"Error generating SQL: {str(e)}"
 
 def main():
-    db_data = load_or_get_data()
-    if db_data is None:
-        print("Exiting program...")
-        return
-
     while True:
-        prompt_text = input("Input your prompt for SQL on how you would like to group your data: ")
-        sql_code = generate_sql(db_data, prompt_text)
-
-        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        with open(f"sql_output_{timestamp}.txt", 'w') as f:
-            f.write(sql_code)
-
-        print(f"Generated SQL code has been written to sql_output_{timestamp}.txt.")
-
-        cont = input("Would you like to continue with the current input data? (yes/no/quit): ").strip().lower()
-        if cont == 'quit':
+        db_data = get_input_data()
+        if db_data[0] is None:
+            print("Exiting program...")
             break
-        elif cont == 'no':
-            if os.path.exists(memory_file):
-                os.remove(memory_file)  # Remove memory file to start fresh
-            db_data = load_or_get_data()
-            if db_data is None:
-                print("Exiting program...")
+
+        while True:
+            prompt_text = input("Input your prompt for SQL on how you would like to group your data (or type 'quit' to exit): ")
+            if prompt_text.lower() == 'quit':
+                break
+
+            sql_code = generate_sql(db_data, prompt_text)
+            timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            with open(f"sql_output_{timestamp}.sql", 'w') as f:
+                f.write(sql_code)
+            print(f"Generated SQL code has been written to sql_output_{timestamp}.sql.")
+            cont = input("Would you like to continue with the current input data? (yes/no/quit): ").strip().lower()
+            if cont == 'quit':
                 return
+            elif cont == 'no':
+                break
 
 if __name__ == "__main__":
+    pycache = load_pycache()
     main()
